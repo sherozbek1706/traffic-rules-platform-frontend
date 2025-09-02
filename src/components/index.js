b7efd4db9e7e4a3beeabcfddf3c08419b9c94350
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2025-09-02 13:18:11 +0500

    componentlarni export qilib yuborish

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export * from "./layout-dashboard";
 export * from "./navbar-dashboard";
 export * from "./sidebar-dashboard";
+export * from "./add-admin-modal";
+export * from "./edit-admin-modal";
+export * from "./layout-student";
+export * from "./navbar-student";
