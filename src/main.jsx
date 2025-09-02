b20431f1ae68be9e67b2c90a65764cb17c2e6522
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2025-09-02 13:17:44 +0500

    main va index.css fayllari

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+// Flowbite JS ulash
+import "flowbite";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
+);
