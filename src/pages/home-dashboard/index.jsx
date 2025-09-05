e9cc220de5f6ce05808ce9a9f351c12e1ab4e869
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2025-09-05 13:21:44 +0500

    dashboardning asosiy sahifasi

---
+++ src/pages/home-dashboard/index.jsx
@@ -0,0 +1,10 @@
+// src/pages/HomeDashboard.jsx
+import { LayoutDashboard } from "../../components";
+
+export const HomeDashboard = () => {
+  return (
+    <LayoutDashboard>
+      <h1>HOME DASHBOARD</h1>
+    </LayoutDashboard>
+  );
+};
