cca2ebecf630ada7cbbcfa511e08c2991b572995
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2025-09-10 13:30:19 +0500

    pagelarni export qilib yuborish

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export * from "./home";
+export * from "./login";
+export * from "./home-dashboard";
+export * from "./login-dashboard";
+export * from "./admins-dashboard";
+export * from "./groups-dashboard";
+export * from "./group-admins-dashboard";
